9b09411b23e6f856e81004fff88981e2a91f6dfe
David Bal <david.bal37@gmail.com>
2016-11-24 20:20:07 -0800

    added no-arg constructor

--- src/main/java/net/balsoftware/internal/scene/control/login/LoginBorderPane.java
+++ src/main/java/net/balsoftware/internal/scene/control/login/LoginBorderPane.java
@@ -1,12 +1,9 @@
 package net.balsoftware.internal.scene.control.login;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -15,6 +12,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.util.Callback;
 import javafx.util.Pair;
+import net.balsoftware.FXMLUtilities;
 
 public class LoginBorderPane extends BorderPane
 {
@@ -26,25 +24,11 @@ public class LoginBorderPane extends BorderPane
     public LoginBorderPane(ResourceBundle resources, String initialUsername, Callback<Pair<String, String>, Void> loginCallback)
     {
         super();
-        loadFxml(LoginBorderPane.class.getResource("Login.fxml"), this, resources);
+        FXMLUtilities.loadFxml(LoginBorderPane.class.getResource("Login.fxml"), this, resources);
         usernameTextField.setText(initialUsername);
         signInButton.setOnMouseClicked((EventHandler<? super MouseEvent>) (event) -> 
         {
             loginCallback.call(new Pair<>(usernameTextField.getText(), passwordField.getText()));
         });
-    }
-    
-    protected static void loadFxml(URL fxmlFile, Object rootController, ResourceBundle resources)
-    {
-        FXMLLoader loader = new FXMLLoader(fxmlFile);
-        loader.setController(rootController);
-        loader.setRoot(rootController);
-        loader.setResources(resources);
-        try {
-            loader.load();
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/test/java/net/balsoftware/scene/control/login/trial/SimpleLoginDemo.java
+++ src/test/java/net/balsoftware/scene/control/login/trial/SimpleLoginDemo.java
@@ -1,9 +1,6 @@
 package net.balsoftware.scene.control.login.trial;
 
 
-import java.util.Locale;
-import java.util.ResourceBundle;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -23,9 +20,7 @@ public class SimpleLoginDemo extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Locale myLocale = Locale.getDefault();
-        ResourceBundle resources = ResourceBundle.getBundle("net.balsoftware.scene.control.login.trial.NewLogin", myLocale);
-        Login root = new Login(resources, null, (p) -> null);
+        Login root = new Login();
         Scene scene = new Scene(root, 300, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
